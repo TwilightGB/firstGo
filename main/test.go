568901427dcebfe258ca8b4d15afd101a4b7ded0
gengbin <gengbin@jd.com>
2018-08-22 10:09:37 +0800

    add text crawl

--- main/test.go
+++ main/test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
+	"bufio"
 	"firstGo/rule"
 	"firstGo/utils"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"os"
 )
 
 type image struct {
-	surl string
-	filepath string
-	page int
+	surl      string
+	filepath  string
+	page      int
 	ImageRule rule.Rule
 }
 
@@ -29,25 +32,59 @@ func (im *image) getImage(surl string) []string {
 //, strconv.FormatInt(time.Now().Unix(), 10)
 //strconv.Itoa(rand.Intn(10)) + ".jpg"
 func main() {
-	var resultList = make([]string,0)
+	ch := make(chan string, 100)
+	go func(ch chan string) {
+		file, error := os.Open("E:\\mygo\\src\\firstGo\\config.properties")
+		if error != nil {
+			panic(error)
+		}
+		defer file.Close()
+		r := bufio.NewReader(file)
+		for {
+			byte, _, error := r.ReadLine()
+			if error != nil {
+				if error == io.EOF {
+					break
+				}
+				panic(error)
+			}
+			ch <- string(byte)
+		}
+	}(ch)
+	f, error := os.Create("E:\\e")
+	if error != nil {
+		panic(error)
+	}
+	defer f.Close()
+	x, ok := <-ch
+	if ok != false {
+		fmt.Println(x)
+	}
+	fmt.Println("=====")
+	dMap := goUtils.InitConfig("E:\\mygo\\src\\firstGo\\config.properties")
+	for key, value := range dMap {
+		fmt.Println(key + "  " + value)
+	}
+
+	var resultList = make([]string, 0)
 	//images:=&image{surl:"",filepath:"",page:1,ImageRule:&rule.Douban{}}
 	//fmt.Println(images)
-	image :=new(image)
-	image.surl="https://book.douban.com/tag/Programming?"
-	image.filepath="D:\\temp\\"
-	image.page=20
-	image.ImageRule=&rule.Douban{}
-	for i:=0;i<3;i++{
-		lnewUrl:=image.ImageRule.PageRule(image.page)
-		result:=image.getImage(image.surl+lnewUrl)
-		for _,datau:= range result{
-			resultList=append(resultList,datau)
+	image := new(image)
+	image.surl = "https://book.douban.com/tag/Programming?"
+	image.filepath = "D:\\temp\\"
+	image.page = 20
+	image.ImageRule = &rule.Douban{}
+	for i := 0; i < 3; i++ {
+		lnewUrl := image.ImageRule.PageRule(image.page)
+		result := image.getImage(image.surl + lnewUrl)
+		for _, datau := range result {
+			resultList = append(resultList, datau)
 		}
-		image.page+=20
+		image.page += 20
 	}
-	for _,data:= range resultList{
+	for _, data := range resultList {
 		fmt.Println(data)
-		sufixx:=image.ImageRule.ParseUrl(data)
-		goUtils.DownLoad(image.filepath+sufixx,data)
+		sufixx := image.ImageRule.ParseUrl(data)
+		goUtils.DownLoad(image.filepath+sufixx, data)
 	}
 }
--- utils/goUtils.go
+++ utils/goUtils.go
@@ -1,14 +1,14 @@
 package goUtils
 
 import (
-	"os"
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"io"
+	"bufio"
 	"bytes"
 	"errors"
-	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strings"
 )
 
@@ -19,8 +19,6 @@ func IsExist(dir string) bool {
 	}
 	return os.IsExist(err)
 }
-
-
 
 func DownLoad(filepath string, url string) error {
 	trueOrFalse := IsExist(filepath)
@@ -48,38 +46,38 @@ func DownLoad(filepath string, url string) error {
 	return nil
 }
 
-func InitConfig(path string) map[string]string  {
+func InitConfig(path string) map[string]string {
 	var resultMap = make(map[string]string)
-	file,error:=os.Open(path)
-	if error!=nil{
+	file, error := os.Open(path)
+	if error != nil {
 		panic(error)
 	}
 	defer file.Close()
-	reader :=bufio.NewReader(file)
-	for{
-		byte,_,error:=reader.ReadLine()
-		if error!=nil{
-			if error==io.EOF{
+	reader := bufio.NewReader(file)
+	for {
+		byte, _, error := reader.ReadLine()
+		if error != nil {
+			if error == io.EOF {
 				break
 			}
 			panic(error)
 		}
-		sLine :=strings.TrimSpace((string(byte)))
+		sLine := strings.TrimSpace((string(byte)))
 		//
-		index:=strings.Index(sLine,":")
-		if index<0{
+		index := strings.Index(sLine, "=")
+		if index < 0 {
 			continue
 		}
 
-		key:=strings.TrimSpace(sLine[:index])
-		if len(key)==0{
+		key := strings.TrimSpace(sLine[:index])
+		if len(key) == 0 {
 			continue
 		}
-		value:=strings.TrimSpace(sLine[index:])
-		if len(value)==0{
+		value := strings.TrimSpace(sLine[index+1 : len(sLine)])
+		if len(value) == 0 {
 			continue
 		}
-		resultMap[key]=value
+		resultMap[key] = value
 	}
-	return  resultMap
+	return resultMap
 }
